9e2d1aeb058d0c996a2807b317629c600da00285
tsizehena223 <tsizehena223@gmail.com>
2025-05-20 23:32:04 +0300

    update lsp

--- lua/lsp.lua
+++ lua/lsp.lua
@@ -3,4 +3,6 @@ local lspconfig = require("lspconfig")
 lspconfig.pyright.setup({})
 lspconfig.intelephense.setup({})
 lspconfig.ts_ls.setup({})
-lspconfig.html.setup({})
+lspconfig.html.setup({
+  filetypes = { "html", "javascriptreact", "typescriptreact" }, -- for JSX/TSX
+})
